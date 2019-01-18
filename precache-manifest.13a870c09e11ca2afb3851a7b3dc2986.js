061243e28b97161f7e20aaffffd6b17ea2420e45
Jenny Kao <k.jennymh@gmail.com>
2019-01-18 11:13:51 +0800

    Updates

---
+++ precache-manifest.13a870c09e11ca2afb3851a7b3dc2986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a8874e6c9886a207804",
+    "url": "/static/css/main.335a619d.chunk.css"
+  },
+  {
+    "revision": "2a8874e6c9886a207804",
+    "url": "/static/js/main.2a8874e6.chunk.js"
+  },
+  {
+    "revision": "cdcc6bc533a05aa3634a",
+    "url": "/static/js/1.cdcc6bc5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "adf940936b26451af510ed9107d901f9",
+    "url": "/index.html"
+  }
+];
